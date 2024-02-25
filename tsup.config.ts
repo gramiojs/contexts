332224828369d4ca0c10c00c2ab0c62f92f8de96
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-02-25 15:28:19 +0300

    feat: update inspectable https://github.com/negezor/inspectable/releases/tag/inspectable%403.0.0 now it use JS decorators

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "tsup";
 
+// ![INFO] esbuild support of decorators... https://github.com/evanw/esbuild/issues/104
 export default defineConfig({
-	target: "esnext",
+	target: "es2022",
 	splitting: true,
 	entry: ["./src/index.ts"],
 	format: ["cjs"],
