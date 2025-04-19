22f205b779f7cfeeb5b5e9c1e367418312071f56
kravetsone <57632712+kravetsone@users.noreply.github.com>
2025-04-19 18:26:12 +0300

    chore: /index imports in barrel file

--- scripts/generate-imports.ts
+++ scripts/generate-imports.ts
@@ -6,7 +6,10 @@ const dirPath = path.resolve(process.cwd(), process.argv[2]);
 fs.readdir(dirPath).then(async (files) => {
 	const lines = files
 		.filter((file) => file !== "index.ts")
-		.map((file) => `export * from "./${file.replace(".ts", "")}";`);
+		.map(
+			(file) =>
+				`export * from "./${file.endsWith(".ts") ? file : `${file}/index.ts`}";`,
+		);
 
 	await fs.writeFile(path.resolve(dirPath, "index.ts"), lines.join("\n"));
 });
